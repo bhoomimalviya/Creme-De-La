80e59837193eeb2a8ec855c97848d6d41ef213bf
bhoomimalviya <bhagwatimalviya180@gmail.com>
2025-10-03 16:21:53 +0530

    Added Snacks Page

--- customer/menu/snacks.php
+++ customer/menu/snacks.php
@@ -9,47 +9,47 @@ if(!isset($_SESSION['user_id'])){
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/Creme-De-La/includes/boilerplate.php"; ?>
 
 <div class="category-page">
-    <h2>Cold Drinks</h2>
+    <h2>Snacks</h2>
     <div class="items-grid">
         
         <div class="item-card">
-            <img src="/Creme-De-La/assets/images/menu-items/blueberry-lemon-drink.jpeg" alt="Blueberry Lemon Drink">
-            <h3>Blueberry Lemon Drink</h3>
+            <img src="/Creme-De-La/assets/images/menu-items/burger-snacks.jpeg" alt="Burger">
+            <h3>Burger</h3>
             <p>₹450</p>
             <button>Add to Cart</button>
         </div>
 
         <div class="item-card">
-            <img src="/Creme-De-La/assets/images/menu-items/iced-coffee-drink.jpeg" alt="Iced Coffee">
-            <h3>Iced Coffee</h3>
+            <img src="/Creme-De-La/assets/images/menu-items/cheeseBalls-snacks.jpeg" alt="Cheese Balls">
+            <h3>Cheese Balls</h3>
             <p>₹500</p>
             <button>Add to Cart</button>
         </div>
 
         <div class="item-card">
-            <img src="/Creme-De-La/assets/images/menu-items/matcha-drink.jpeg" alt="Matcha Drink">
-            <h3>Matcha Drink</h3>
+            <img src="/Creme-De-La/assets/images/menu-items/fries-snacks.jpeg" alt="Peri Peri Fries">
+            <h3>Peri Peri Fries</h3>
             <p>₹500</p>
             <button>Add to Cart</button>
         </div>
 
         <div class="item-card">
-            <img src="/Creme-De-La/assets/images/menu-items/mocha-drink.jpeg" alt="Mocha Drink">
-            <h3>Mocha Drink</h3>
+            <img src="/Creme-De-La/assets/images/menu-items/grilledCheeseSandwich-snacks.jpeg" alt="Grilled Cheese Sandwich">
+            <h3>Grilled Cheese Sandwich</h3>
             <p>₹500</p>
             <button>Add to Cart</button>
         </div>
 
         <div class="item-card">
-            <img src="/Creme-De-La/assets/images/menu-items/vanilla-chai-latte[special].jpeg" alt="Vanilla Chai Latte [Special]">
-            <h3>Vanilla Chai Latte [Special]</h3>
+            <img src="/Creme-De-La/assets/images/menu-items/loadedNachos-snacks.jpeg" alt="Loaded Nachos">
+            <h3>Loaded Nachos</h3>
             <p>₹500</p>
             <button>Add to Cart</button>
         </div>
 
         <div class="item-card">
-            <img src="/Creme-De-La/assets/images/menu-items/violet-drink.jpeg" alt="Violet Drink">
-            <h3>Violet Drink</h3>
+            <img src="/Creme-De-La/assets/images/menu-items/paneer-roll-snacks.jpeg" alt="Paneer Roll">
+            <h3>Paneer Roll</h3>
             <p>₹500</p>
             <button>Add to Cart</button>
         </div>
